59feead8d0cd5bb8de836a6f38f384780637dc82
abhinav.mishra <abhinav.mishra@myntra.com>
2023-02-27 17:23:50 +0530

    Initial commit

---
+++ src/test/java/com/store/locator/storeLocator/StoreLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.locator.storeLocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreLocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
